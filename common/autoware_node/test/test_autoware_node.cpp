fde41775dd62dd2c5db11629d191c1e627925f84
M. Fatih Cırıt <mfc@leodrive.ai>
2022-09-26 19:13:06 +0300

    add copyright info
    Signed-off-by: M. Fatih Cırıt <mfc@leodrive.ai>

--- common/autoware_node/test/test_autoware_node.cpp
+++ common/autoware_node/test/test_autoware_node.cpp
@@ -13,23 +13,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "autoware_node/autoware_node.hpp"
-
-namespace autoware_node
-{
-
-AutowareNode::AutowareNode(
-  const std::string & nodeName, const rclcpp::NodeOptions & options,
-  bool enableCommunicationInterface)
-: LifecycleNode(nodeName, options, enableCommunicationInterface)
-{
-}
-
-AutowareNode::AutowareNode(
-  const std::string & node_name, const std::string & namespace_,
-  const rclcpp::NodeOptions & options, bool enable_communication_interface)
-: LifecycleNode(node_name, namespace_, options, enable_communication_interface)
-{
-}
-
-}  // namespace autoware_node
+#include "test_autoware_node.hpp"
--- common/autoware_node/test/test_autoware_node.hpp
+++ common/autoware_node/test/test_autoware_node.hpp
@@ -13,23 +13,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "autoware_node/autoware_node.hpp"
+#ifndef TEST_AUTOWARE_NODE_HPP_
+#define TEST_AUTOWARE_NODE_HPP_
 
-namespace autoware_node
+class test_autoware_node
 {
+};
 
-AutowareNode::AutowareNode(
-  const std::string & nodeName, const rclcpp::NodeOptions & options,
-  bool enableCommunicationInterface)
-: LifecycleNode(nodeName, options, enableCommunicationInterface)
-{
-}
-
-AutowareNode::AutowareNode(
-  const std::string & node_name, const std::string & namespace_,
-  const rclcpp::NodeOptions & options, bool enable_communication_interface)
-: LifecycleNode(node_name, namespace_, options, enable_communication_interface)
-{
-}
-
-}  // namespace autoware_node
+#endif  // TEST_AUTOWARE_NODE_HPP_
